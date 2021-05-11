6711718887b16a2eb15fd9db4fbe6047d4251d0e
David Goren <david.goren_ex@scotiabank.cl>
2021-05-11 14:51:15 -0400

    Initial Commit

---
+++ MapKitTutorial/MapKitTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitTutorialApp.swift
+//  MapKitTutorial
+//
+//  Created by usuario on 11-05-21.
+//
+
+import SwiftUI
+
+@main
+struct MapKitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
